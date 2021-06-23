c187c1fe06634c48a59f3d633457bae5b11862ee
washucode <estherw8525@gmail.com>
2021-06-23 18:24:29 +0300

    Add IFTTT  and Crypto resource api

--- bitcoin_notifications.py
+++ bitcoin_notifications.py
@@ -2,6 +2,8 @@ import requests
 import time
 from datetime import datetime
 
+BITCOIN_API_URL = 'https://api.coinlore.net/api/tickers/'
+IFTTT_WEBHOOKS_URL = 'https://maker.ifttt.com/trigger/{}/with/key/uWWPIMHdI8GgL7Ysisimk'
 def main():
     pass
 
